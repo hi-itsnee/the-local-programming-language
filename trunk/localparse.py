e86a666fff5285e6228d7dfbbbd20f981f4c9faf
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-03-28 21:39:57 +0000

    initial import of statements

--- trunk/localparse.py
+++ trunk/localparse.py
@@ -2,11 +2,22 @@
 # Author:                  Team 13
 # Description:             The local programming language parser
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-03-21 16:59:37 mc>
+# Time-stamp:              <2012-03-28 17:33:27 mc>
 
 import ply.yacc as yacc
 import locallex
 from io_statement import *
+from string_statement import *
+from list_statement import *
+from except_statement import *
+from coord_statement import *
+from iter_statement import *
+from cond_statement import *
+from math_statement import *
+from logic_statement import *
+from assign_statement import *
+from def_statement import *
+from exit_statement import *
 
 # Enable/disable debugging
 DEBUG = True
@@ -26,7 +37,19 @@ precedence = (
 
 def p_program(p):
     '''program : program io_statement
-               | io_statement'''
+               | io_statement
+               | string_statement
+               | list_statement
+               | except_statement
+               | coord_statement
+               | iter_statement
+               | cond_statement
+               | math_statement
+               | logic_statement
+               | assign_statement
+               | def_statement
+               | exit_statement
+               '''
 
 # Error handler. Return nothing.
 def p_program_error(p):
@@ -47,3 +70,9 @@ def parse(data, debug=DEBUG):
     if lparser.error:
         return None
     return p
+Welcome to Ubuntu 11.10 (GNU/Linux 3.0.0-16-generic x86_64)
+
+ * Documentation:  https://help.ubuntu.com/
+Last login: Wed Mar 28 17:15:38 2012 from 172.16.248.1
+plt@plt:~$ tramp_exit_status 0
+///e93b924d0f68d29db0ed3160dbf7d71a#$
