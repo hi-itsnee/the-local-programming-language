66b932129edf0fd75de6fa6bcfde8acb75055fc8
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-03-29 00:11:18 +0000

    moved print out of here. added open

--- trunk/io_statement.py
+++ trunk/io_statement.py
@@ -1,15 +1,22 @@
 # Filename:                io_statement.py
 # Author:                  Team 13
-# Description:             The local programming language parser I/O statements
+# Description:             local parser I/O statements
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-03-21 16:59:03 mc>
+# Time-stamp:              <2012-03-28 20:04:13 plt>
 
 from localparse_util import print_code
 
 def p_io_statement(p):
-    '''io_statement : print_statement'''
+    '''io_statement : open_statement'''
 
-def p_print_statement(p):
-    '''print_statement : PRINT LPAREN STRING RPAREN SEMI'''
-    code = "print \"%s\"" % p[3]
+def p_open_statement(p):
+    '''open_statement : OPEN LPAREN STRING RPAREN SEMI
+                      | OPEN LPAREN STRING COMMA STRING RPAREN SEMI'''
+    filename = p[3]
+    if len(p) == 6:
+        # Default is read (r)
+        mode = "r"
+    elif len(p) == 8:
+        mode = p[5]
+    code = "open(\"%s\", \"%s\")" % (filename, mode)
     print_code(code, 0)
