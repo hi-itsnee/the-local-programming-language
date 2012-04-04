952221fda4017be6d30aaab9d115efaaaacaee3c
Julianna.Eum <Julianna.Eum@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-04 21:40:30 +0000

    updated math statement, but it gets errors in parsing

---
+++ test/checkassign.py
@@ -0,0 +1,14 @@
+#type: program, value: None
+#type: statement_list, value: None
+#type: statement_list, value: None
+#type: statement_list, value: None
+#type: statement_list, value: None
+#type: statement, value: None
+#type: expr, value: SYNTAX
+#type: statement, value: None
+#type: expr, value: ERROR
+#type: statement, value: None
+#type: expr, value: AT
+#type: statement, value: None
+#type: expr, value: EOF
+EOF
