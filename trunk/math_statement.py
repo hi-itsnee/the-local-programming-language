3323ef10200343cabc07e6fac257e804ea9984e0
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-04 21:54:41 +0000

    fixed basic math parsing

--- trunk/math_statement.py
+++ trunk/math_statement.py
@@ -2,52 +2,46 @@
 # Author:                Team 13
 # Description:           local parser math statements
 # Supported Language(s): Python 2.x
-# Time-stamp:            <2012-04-04 10:00:00>
+# Time-stamp:            <2012-04-04 17:53:48 plt>
 
 from localast import Node
 
-def p_math_statement(p): 
-    '''math_statement : ID EQUALS math_expr''' 
-
-    value = "%s = " % (p[1])
-    p[0] = Node("math_expr", p[1], value, None)
+def p_math_statement(p):
+    '''math_statement : ID EQUALS math_expr SEMI'''
+    line = p[1] + " = %s"
+    p[0] = Node("math_expr", [p[3]], None, line)
 
 
 def p_math_expr(p):
-    '''math_expr : NUMBER PLUS NUMBER SEMI
-                 | NUMBER MINUS NUMBER SEMI
-                 | NUMBER TIMES NUMBER SEMI
-                 | NUMBER DIVIDE NUMBER SEMI
-                 | NUMBER POWER NUMBER SEMI
-                 | ID'''
+    '''math_expr : NUMBER PLUS NUMBER
+                 | NUMBER MINUS NUMBER
+                 | NUMBER TIMES NUMBER
+                 | NUMBER DIVIDE NUMBER
+                 | NUMBER POWER NUMBER'''
 
     if p[2] == '+':
-        if (p[1] == ID and p[3] == ID):
-            p[0] = Node("math", [p[1], p[3]], None, "%s + %s;\n")
-        elif (p[1] == NUMBER and p[3] == NUMBER):
-            p[0] = p[1] + p[3]
-	else: print "The plus operator received bad inputs\n"
-    elif p[2] == '-':
-        if (p[1] == ID and p[3] == ID):
-            p[0] = Node("math", [p[1], p[3]], None, "%s - %s;\n")
-        elif (p[1] == NUMBER and p[3] == NUMBER):
-            p[0] = p[1] - p[3]
-	else: print 'The minus operator received bad inputs\n'	    
-    elif p[2] == '*':
-        if (p[1] == ID and p[3] == ID):
-            p[0] = Node("math", [p[1], p[3]], None, "%s * %s;\n")
-        elif (p[1] == NUMBER and p[3] == NUMBER):
-            p[0] = p[1] * p[3]
-	else: print 'The times operator received bad inputs\n'
-    elif p[2] == '/':
-        if (p[1] == ID and p[3] == ID):
-            p[0] = Node("math", [p[1], p[3]], None, "%s / %s;\n")
-        elif (p[1] == NUMBER and p[3] == NUMBER):
-            p[0] = p[1] / p[3]
-	else: print 'The divide operator received bad inputs\n'
-    elif p[2] == '^':
-        if (p[1] == ID and p[3] == ID):
-            p[0] = Node("math", [p[1], p[3]], None, "%s ^ %s;\n")
-        elif (p[1] == NUMBER and p[3] == NUMBER):
-            p[0] = p[1] ** p[3]
-	else: print 'The power operator received bad inputs\n'
+        p[0] = Node("plus", None, "%s + %s" % (p[1], p[3]))
+    # elif p[2] == '-':
+    #     if (p[1] == ID and p[3] == ID):
+    #         p[0] = Node("math", [p[1], p[3]], None, "%s - %s;\n")
+    #     elif (p[1] == NUMBER and p[3] == NUMBER):
+    #         p[0] = p[1] - p[3]
+    #     else: print 'The minus operator received bad inputs\n'	    
+    # elif p[2] == '*':
+    #     if (p[1] == ID and p[3] == ID):
+    #         p[0] = Node("math", [p[1], p[3]], None, "%s * %s;\n")
+    #     elif (p[1] == NUMBER and p[3] == NUMBER):
+    #         p[0] = p[1] * p[3]
+    #     else: print 'The times operator received bad inputs\n'
+    # elif p[2] == '/':
+    #     if (p[1] == ID and p[3] == ID):
+    #         p[0] = Node("math", [p[1], p[3]], None, "%s / %s;\n")
+    #     elif (p[1] == NUMBER and p[3] == NUMBER):
+    #         p[0] = p[1] / p[3]
+    #     else: print 'The divide operator received bad inputs\n'
+    # elif p[2] == '^':
+    #     if (p[1] == ID and p[3] == ID):
+    #         p[0] = Node("math", [p[1], p[3]], None, "%s ^ %s;\n")
+    #     elif (p[1] == NUMBER and p[3] == NUMBER):
+    #         p[0] = p[1] ** p[3]
+    #     else: print 'The power operator received bad inputs\n'
