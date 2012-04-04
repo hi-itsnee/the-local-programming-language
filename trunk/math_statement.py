f80f6ba4cbe59ca501cfbe0f8276cb713bd5b678
Julianna.Eum <Julianna.Eum@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-04 21:40:30 +0000

    updated math statement, but it gets errors in parsing

--- trunk/math_statement.py
+++ trunk/math_statement.py
@@ -7,22 +7,47 @@
 from localast import Node
 
 def p_math_statement(p): 
-# TO DO: FIX FOR ORDER OF PRECEDENCE
-#        FIX FOR NUMBERS ALSO INSTEAD OF ONLY IDs
-    '''math_statement : ID EQUALS ID POWER ID SEMI
-                      | ID EQUALS ID PLUS ID SEMI
-                      | ID EQUALS ID MINUS ID SEMI
-                      | ID EQUALS ID TIMES ID SEMI
-                      | ID EQUALS ID DIVIDE ID SEMI'''
+    '''math_statement : ID EQUALS math_expr''' 
+
+    value = "%s = " % (p[1])
+    p[0] = Node("math_expr", p[1], value, None)
 
 
-    if p[4] == "POWER":
-        p[0] = Node("math", None, pow(p[3], p[5]))
-    elif p[4] == "PLUS":
-        p[0] = Node("math", None, p[3] + p[5])
-    elif p[4] == "MINUS":
-        p[0] = Node("math", None, p[3] - p[5])  
-    elif p[4] == "TIMES":
-        p[0] = Node("math", None, p[3] * p[5])
-    elif p[4] == "DIVIDE":
-        p[0] = Node("math", None, p[3] / p[5])    
+def p_math_expr(p):
+    '''math_expr : NUMBER PLUS NUMBER SEMI
+                 | NUMBER MINUS NUMBER SEMI
+                 | NUMBER TIMES NUMBER SEMI
+                 | NUMBER DIVIDE NUMBER SEMI
+                 | NUMBER POWER NUMBER SEMI
+                 | ID'''
+
+    if p[2] == '+':
+        if (p[1] == ID and p[3] == ID):
+            p[0] = Node("math", [p[1], p[3]], None, "%s + %s;\n")
+        elif (p[1] == NUMBER and p[3] == NUMBER):
+            p[0] = p[1] + p[3]
+	else: print "The plus operator received bad inputs\n"
+    elif p[2] == '-':
+        if (p[1] == ID and p[3] == ID):
+            p[0] = Node("math", [p[1], p[3]], None, "%s - %s;\n")
+        elif (p[1] == NUMBER and p[3] == NUMBER):
+            p[0] = p[1] - p[3]
+	else: print 'The minus operator received bad inputs\n'	    
+    elif p[2] == '*':
+        if (p[1] == ID and p[3] == ID):
+            p[0] = Node("math", [p[1], p[3]], None, "%s * %s;\n")
+        elif (p[1] == NUMBER and p[3] == NUMBER):
+            p[0] = p[1] * p[3]
+	else: print 'The times operator received bad inputs\n'
+    elif p[2] == '/':
+        if (p[1] == ID and p[3] == ID):
+            p[0] = Node("math", [p[1], p[3]], None, "%s / %s;\n")
+        elif (p[1] == NUMBER and p[3] == NUMBER):
+            p[0] = p[1] / p[3]
+	else: print 'The divide operator received bad inputs\n'
+    elif p[2] == '^':
+        if (p[1] == ID and p[3] == ID):
+            p[0] = Node("math", [p[1], p[3]], None, "%s ^ %s;\n")
+        elif (p[1] == NUMBER and p[3] == NUMBER):
+            p[0] = p[1] ** p[3]
+	else: print 'The power operator received bad inputs\n'
