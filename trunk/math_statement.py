0e253c7459aadfbebdc132794b588e5bbf3ba8d2
Julianna.Eum <Julianna.Eum@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-05 21:03:22 +0000

    fixed some of the problems with the math statement, but it's not parsing the math-test2.local file correctly...

--- trunk/math_statement.py
+++ trunk/math_statement.py
@@ -17,31 +17,20 @@ def p_math_expr(p):
                  | NUMBER MINUS NUMBER
                  | NUMBER TIMES NUMBER
                  | NUMBER DIVIDE NUMBER
+                 | NUMBER MODULO NUMBER
                  | NUMBER POWER NUMBER'''
 
     if p[2] == '+':
         p[0] = Node("plus", None, "%s + %s" % (p[1], p[3]))
-    # elif p[2] == '-':
-    #     if (p[1] == ID and p[3] == ID):
-    #         p[0] = Node("math", [p[1], p[3]], None, "%s - %s;\n")
-    #     elif (p[1] == NUMBER and p[3] == NUMBER):
-    #         p[0] = p[1] - p[3]
-    #     else: print 'The minus operator received bad inputs\n'	    
-    # elif p[2] == '*':
-    #     if (p[1] == ID and p[3] == ID):
-    #         p[0] = Node("math", [p[1], p[3]], None, "%s * %s;\n")
-    #     elif (p[1] == NUMBER and p[3] == NUMBER):
-    #         p[0] = p[1] * p[3]
-    #     else: print 'The times operator received bad inputs\n'
-    # elif p[2] == '/':
-    #     if (p[1] == ID and p[3] == ID):
-    #         p[0] = Node("math", [p[1], p[3]], None, "%s / %s;\n")
-    #     elif (p[1] == NUMBER and p[3] == NUMBER):
-    #         p[0] = p[1] / p[3]
-    #     else: print 'The divide operator received bad inputs\n'
-    # elif p[2] == '^':
-    #     if (p[1] == ID and p[3] == ID):
-    #         p[0] = Node("math", [p[1], p[3]], None, "%s ^ %s;\n")
-    #     elif (p[1] == NUMBER and p[3] == NUMBER):
-    #         p[0] = p[1] ** p[3]
-    #     else: print 'The power operator received bad inputs\n'
+    elif p[2] == '-':
+        p[0] = Node("minus", None, "%s - %s" % (p[1], p[3]))
+    elif p[2] == '*':
+        p[0] = Node("times", None, "%s * %s" % (p[1], p[3]))
+    elif p[2] == '/':
+        p[0] = Node("divide", None, "%s / %s" % (p[1], p[3]))
+    elif p[2] == '%':
+        p[0] = Node("modulo", None, "%s % %s" % (p[1], p[3]))
+    elif p[2] == '^':
+        p[0] = Node("power", None, "%s ** %s" % (p[1], p[3]))
+    else: 
+        print 'Error in math expression\n'
