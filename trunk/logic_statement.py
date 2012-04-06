a2a349a8068ee86b1c38cf1d23322e1d67fe5e79
neerajaramanan@gmail.com <neerajaramanan@gmail.com@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-06 18:44:18 +0000

    Fixed logic expressions - needs to be tied to expressions

--- trunk/logic_statement.py
+++ trunk/logic_statement.py
@@ -6,39 +6,30 @@
 from localast import Node
 
 def p_logic_statement(p):
-    '''logic_statement : ID EQUALS logic_expr SEMI'''
+    '''logic_statement : logic_statement OR logic_term
+		       | logic_term'''
+    if len(p) == 2:
+        p[0] = Node("logic_statement",[p[1]], None, "%s")
+    elif len(p) == 4:
+        p[0] = Node("logic_statement",[p[1],p[3]], None, "%s or %s")
 
-    value = "%s = " % (p[1]) 
-    p[0] = Node("logic_statement",[p[3]], value, None)
+def p_logic_term(p):
+    '''logic_term : logic_term AND logic_factor
+		  | logic_factor'''
 
-def p_logic_expr(p):
-    '''logic_expr : and_statement
-		  | or_statement
-                  | not_statement
-                  | end_statement'''
+    if len(p) == 2:
+        p[0] = Node("logic_term",[p[1]],None,"%s")
+    elif len(p) == 4:
+        p[0] = Node("logic_expr",[p[1],p[3]],None,"%s and %s")
 
-    p[0] = Node("logic_expr",[p[1]],None,None)
-
-def p_end_statement(p):
-    '''end_statement : ID '''
-
-    value = "%s" % (p[1])
-    p[0] = Node("end_statement",None, value, value)
-
-# AND
-def p_and_statement(p):
-    '''and_statement : ID AND logic_expr '''
-    value = "%s and " % (p[1])
-    p[0] = Node("and", [p[3]], value , None) 
-
-#OR
-def p_or_statement(p):
-    '''or_statement : ID OR logic_expr '''
-    value = "%s or " % (p[1])
-    p[0] = Node("or", [p[3]], value, None)
-
-#NOT
-def p_not_statement(p):
-    '''not_statement : NOT logic_expr '''
-
-    p[0] = Node("not", [p[2]], "not", None)  
+def p_logic_factor(p):
+    '''logic_factor : LPAREN logic_statement RPAREN
+		    | NOT logic_factor
+		    | ID '''
+    if len(p) == 4:
+	p[0] = Node("logic_factor",[p[2]],None,"(%s)")
+    elif len(p) == 3:
+	p[0] = Node("logic_factor",[p[2]],None,"not %s")
+    elif len(p) == 2:
+	value = "%s" % (p[1])
+    	p[0] = Node("logic_factor",None, value, value)
