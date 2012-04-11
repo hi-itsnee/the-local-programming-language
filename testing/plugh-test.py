688191b67966ee7f8429dca8b46de28f60fc884c
Julianna.Eum <Julianna.Eum@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-11 20:18:34 +0000

    updated the testing folder

---
+++ testing/plugh-test.py
@@ -0,0 +1,31 @@
+import unittest
+from plugh import *
+
+# RUN: python -m unittest plugh-test.PlughUT.test_joy
+# RUN: python -m unittest plugh-test
+
+class PlughUT(unittest.TestCase):
+    def setUp(self):
+	print "Get ready"
+
+    def test_happy(self):
+	target_result = open('happy.test').read().strip()
+	actual_result = happy()
+	self.assertEqual(target_result, actual_result)
+
+    def test_joy(self):
+	target_result = open('joy.test').read().strip()
+	actual_result = joy()
+	self.assertEqual(target_result, actual_result)
+
+class PlughBadUT(unittest.TestCase):
+    def setUp(self):
+	print "2nd class"
+
+    def test_happy2(self):
+	target_result = open('happy.test').read().strip()
+	actual_result = happy()
+	self.assertEqual(target_result, actual_result)
+
+if __name__ == "__main__":
+    unittest.main()
