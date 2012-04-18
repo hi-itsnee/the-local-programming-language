0247ec7a4dbb911377f39cc1ebd3c07f9ec73be1
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-18 15:00:19 +0000

    Indentation now works. line-parsing for expressions is broken, so it's currently commented out

--- trunk/localast.py
+++ trunk/localast.py
@@ -2,7 +2,7 @@
 # Author:                  Team 13
 # Description:             local language AST utilities
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-04-10 10:59:45 plt>
+# Time-stamp:              <2012-04-18 10:56:53 plt>
 
 # Number of spaces a tab equals
 INDENT = 4
@@ -28,34 +28,52 @@ class Node:
         self.value = value
         self.line = line
 
-# Function to walk the AST
-def walk_the_tree(node, code="", indent=0, debug=False):
+def walk_the_tree(node, code="", debug=False):
+    '''Walk the AST and return a string, which is a Python program'''
     # Default case (should not be hit)
     if node is None:
         return
     # Debugging to watch tree traversal
     if debug:
         print "#type: %s, value: %s" % (node.type, node.value)
+    # Certain statements require substatements to be indented
+    if node.type in indent_them:
+        if node.type == 'if':
+            # Walk tree to build expression
+            expr = walk_the_tree(node.children[0], debug)
+            # Walk tree to build statement and split statement on newlines
+            if_stmt = walk_the_tree(node.children[1], debug).split("\n")
+            if_stmt_i = ""
+            # Add indents to all the lines in the statement
+            for line in if_stmt:
+                if_stmt_i += " "*INDENT + line + "\n"
+            # Construct the if statment
+            code = "if %s:\n%s" % (expr, if_stmt_i)
+        if node.type == 'else':
+            expr = walk_the_tree(node.children[0], debug)
+            if_stmt = walk_the_tree(node.children[1], debug).split("\n")
+            else_stmt = walk_the_tree(node.children[2], debug).split("\n")
+            if_stmt_i = ""
+            else_stmt_i = ""
+            for line in if_stmt:
+                if_stmt_i += " "*INDENT + line + "\n"
+            for line in else_stmt:
+                else_stmt_i += " "*INDENT + line + "\n"
+            code = "if %s:\n%s\nelse:\n%s" % (expr, if_stmt_i, else_stmt_i)
     # If we need to synthesize a string, build a tuple from the subtree
-    if node.line:
-        values = ()
-        # Ident our children if we're the start of a new block
-        if node.type in indent_them:
-            indent += INDENT
-        for child in node.children:
-            values = values + (walk_the_tree(child, code, indent, debug),)
-        # Return to our level if we've indented our children
-        if node.type in indent_them:
-            indent -= INDENT
-        code = code + " "*indent + (node.line % values)
+    # if node.line:
+    #     print "GOT A LINE: " + node.line + node.type
+    #     values = ( )
+    #     # Visit the children
+    #     for child in node.children:
+    #         values = values + (walk_the_tree(child, code, debug),)
+    #     code = code + (node.line % values)
     # Otherwise, just walk the tree
     else:
         for child in node.children:
-            code = walk_the_tree(child, code, indent, debug)
+            code = walk_the_tree(child, code, debug)
     # Set the return value
     if node.value:
-        if node.type in no_indent:
-            indent = 0
-        code = " "*indent + node.value
+        code = node.value
     # Return statement for the function. Builds target code in order
     return code
