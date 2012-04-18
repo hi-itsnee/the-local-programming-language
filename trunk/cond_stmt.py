c93e136cf424127d9ddf6105cbb0cd29c212a77b
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-18 15:01:01 +0000

    removed line values for new ast walking with indentation

--- trunk/cond_stmt.py
+++ trunk/cond_stmt.py
@@ -2,33 +2,33 @@
 # Author:                  Team 13
 # Description:             local language parser conditional statements
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-04-09 22:10:04 plt>
+# Time-stamp:              <2012-04-18 10:54:28 plt>
 
 from localast import Node
-# Node(type, children=None, value=None, indent_next=False)
+# Node(type, children=None, value=None, line=None)
 
 def p_cond_stmt(p):
-    '''cond_stmt : if_statement
-                 | if_else_statement'''
+    '''cond_stmt : if_stmt
+                 | if_else_stmt'''
                       # | if_elif_statement
                       # | if_elif_else_statement
-    p[0] = Node("cond_statement", [p[1]])
+    p[0] = Node("cond_stmt", [p[1]])
 
 # IF
-def p_if_statement(p):
-    '''if_statement : if_simple
-                    | if_lbrace'''
+def p_if_stmt(p):
+    '''if_stmt : if_simple
+               | if_lbrace'''
                     # | if_simple_parens
                     # | if_lbrace_parens'''
-    p[0] = Node("if_statement", [p[1]])
+    p[0] = Node("if_stmt", [p[1]])
 
 def p_if_simple(p):
     '''if_simple : IF expr_stmt stmt_list %prec IFX'''
-    p[0] = Node("if", [p[2], p[3]], None, "if %s:\n%s")
+    p[0] = Node("if", [p[2], p[3]], None, None)
 
 def p_if_lbrace(p):
     '''if_lbrace : IF expr_stmt LBRACE stmt_list RBRACE %prec IFX'''
-    p[0] = Node("if", [p[2], p[4]], None, "if %s:\n%s")
+    p[0] = Node("if", [p[2], p[4]], None, None)
 
 # def p_if_simple_parens(p):
 #     '''if_simple_parens : IF LPAREN expression RPAREN statement_list'''
@@ -85,20 +85,20 @@ def p_if_lbrace(p):
 
 
 # ELSE
-def p_if_else_statement(p):
-    '''if_else_statement : if_else_simple
-                         | if_else_lbrace'''
+def p_if_else_stmt(p):
+    '''if_else_stmt : if_else_simple
+                    | if_else_lbrace'''
 #                       | else_rbrace
 #                       | else_rlbrace'''
-    p[0] = Node("if_else_statement", [p[1]])
+    p[0] = Node("if_else_stmt", [p[1]])
 
 def p_if_else_simple(p):
     '''if_else_simple : IF expr_stmt stmt_list ELSE stmt_list'''
-    p[0] = Node("else", [p[2], p[3], p[5]], None, "if %s:\n%s\nelse:\n%s")
+    p[0] = Node("else", [p[2], p[3], p[5]], None, None)
 
 def p_if_else_lbrace(p):
     '''if_else_lbrace : IF expr_stmt LBRACE stmt_list RBRACE ELSE stmt_list'''
-    p[0] = Node("else", [p[2], p[4], p[7]], None, "if %s:\n%s\nelse:\n%s")
+    p[0] = Node("else", [p[2], p[4], p[7]], None, None)
 
 # def p_else_rbrace(p):
 #     '''else_rbrace : RBRACE ELSE statement_list'''
