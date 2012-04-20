5797bf3dc2b379c4770a9ae496232417230c316f
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-20 18:54:48 +0000

    list functions

--- trunk/def_stmt.py
+++ trunk/def_stmt.py
@@ -2,11 +2,11 @@
 # Author:                  Team 13
 # Description:             local language parser exception statements
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-04-20 12:40:04 plt>
+# Time-stamp:              <2012-04-20 14:41:57 plt>
 
 from localast import Node
 # Node(type, children=None, value=None, line=None)
 
 def p_def_stmt(p):
-    '''def_stmt : DEF ID LPAREN ID RPAREN LBRACE stmt_list RBRACE
-                | DEF ID LPAREN ID RPAREN LBRACE PASS SEMI RBRACE'''
+    '''def_stmt : DEF ID LPAREN atom RPAREN LBRACE stmt_list RBRACE
+                | DEF ID LPAREN atom RPAREN LBRACE PASS SEMI RBRACE'''
