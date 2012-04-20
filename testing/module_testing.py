9c77f7bf580ea3a809ec6d6fd4385e1e69594be5
Julianna.Eum <Julianna.Eum@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-20 15:53:56 +0000

    Added more tests

--- testing/module_testing.py
+++ testing/module_testing.py
@@ -7,8 +7,8 @@ import local
 import testinputs
 import testoutputs
 
-# RUN: python -m unittest module-testing.TestCond.test_if
-# RUN: python -m unittest module-testing
+# RUN: python -m unittest module_testing.TestCond.test_if
+# RUN: python -m unittest module_testing
 
 class TestCond(unittest.TestCase):
     def setUp(self):
@@ -27,6 +27,12 @@ class TestCond(unittest.TestCase):
 	actual_result = local.tester(testinputs.ifelse_test)
 	self.assertEqual(target_result, actual_result)
 
+    def test_nesting(self):
+    # Test a nested if-else within an if-else
+        target_result = testoutputs.nesting_test
+	actual_result = local.tester(testinputs.nesting_test)
+	self.assertEqual(target_result, actual_result)        
+
 class TestMath(unittest.TestCase):
     def setUp(self):
         print "Begin Math Testing"
@@ -36,6 +42,42 @@ class TestMath(unittest.TestCase):
         target_result = testoutputs.math_test1
         actual_result = local.tester(testinputs.math_test1)
 	self.assertEqual(target_result, actual_result)
+
+class TestAssign(unittest.TestCase):
+    def setUp(self):
+        print "Begin Assignment Testing"
+
+    def test_assign(self):
+        target_result = testoutputs.assign_test
+        actual_result = local.tester(testinputs.assign_test)
+	self.assertEqual(target_result, actual_result)        
+
+class TestExit(unittest.TestCase):
+    def setUp(self):
+        print "Begin Exit Testing"
+
+    def test_exit(self):
+        target_result = testoutputs.exit_test
+        actual_result = local.tester(testinputs.exit_test)
+	self.assertEqual(target_result, actual_result)        
+
+class TestHelloWorld(unittest.TestCase):
+    def setUp(self):
+        print "Begin Hello World Test"
+
+    def test_hw(self):
+        target_result = testoutputs.helloworld_test
+        actual_result = local.tester(testinputs.helloworld_test)
+	self.assertEqual(target_result, actual_result)  
+
+class Example1(unittest.TestCase):
+    def setUp(self):
+        print "Begin Example 1"
+
+    def test_example1(self):
+        target_result = testoutputs.example1_test
+        actual_result = local.tester(testinputs.example1_test)
+	self.assertEqual(target_result, actual_result)    
 
 if __name__ == '__main__':
     unittest.main()
--- testing/testinputs.py
+++ testing/testinputs.py
@@ -1,4 +1,7 @@
-# cond_stmt.py local tests:
+##### Input .local files for testing
+
+### LEXING/PARSING TESTS:
+# cond_stmt.py local test inputs:
 if_test = '''if a
     print("a is true");'''
 
@@ -7,5 +10,35 @@ ifelse_test = '''if a
 else
     print("a is false");'''
 
-# math_expr.py inputs:
-math_test1 ='''a = isd + dsa * 12 + (7/5);'''
+nesting_test = '''if a {
+    print("a is true");
+    if b
+        print("made it here");
+    else
+        print("made it there");
+}
+else
+    print("a is false");'''
+
+# math_expr.py local test inputs:
+math_test1 = '''a = isd + dsa * 12 + (7/5);'''
+
+# assign_stmt.py local test inputs:
+assign_test = '''a = 10;
+b = 0;'''
+
+# exit
+exit_test = '''exit(1);'''
+
+
+### BLACK BOX TESTS:
+# Hello World:
+helloworld_test = '''print("Hello, world!");'''
+
+# example1.py local test inputs:
+example1_test = '''x = 1;
+y = 2;
+z = 1.1;
+w1 = x + y;
+w2 = x - y;
+w3 = x + z;'''
