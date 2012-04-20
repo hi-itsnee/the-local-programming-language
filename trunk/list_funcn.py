7bcbd42f1c4ac8d5730899f6b593f308ab581fc9
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-20 23:10:34 +0000

    redid list functions.  add LIST type to atom (commented out)

--- trunk/list_funcn.py
+++ trunk/list_funcn.py
@@ -2,68 +2,46 @@
 # Author:                  Team 13
 # Description:             local language parser list functions
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-04-15 22:56:17 plt>
+# Time-stamp:              <2012-04-20 19:07:10 plt>
 
 from localast import Node
 # Node(type, children=None, value=None, line=None)
 
 def p_list_funcn(p):
     '''list_funcn : append
-                  | remove
-                  | pop'''
+                  | remove_generic
+                  | pop_generic'''
     p[0] = Node("list_funcn", [p[1]])
 
-# APPEND
-def p_append_gen(p):
-    '''append : append_str
-              | append_other'''
-    p[0] = Node("append", [p[1]])
+# APPEND - Appends item to end of list
+def p_append(p):
+    '''append : APPEND LPAREN atom COMMA atom RPAREN SEMI'''
+    p[0] = Node("append", [p[3], p[5]])
+    # MUST ADD AST ACTION
 
-def p_append_str(p):
-    '''append_str : APPEND LPAREN ID COMMA STRING RPAREN SEMI'''
-    value = "%s.append(\"%s\")" % (p[3], p[5])
-    p[0] = Node("append", None, value)
+# REMOVE - Removes (and returns) specified item from list
+def p_remove_generic(p):
+    '''remove_generic : remove
+                      | remove SEMI'''
+    p[0] = Node("remove_generic", [p[1]])
 
-def p_append_other(p):
-    '''append_other : APPEND LPAREN ID COMMA ID RPAREN SEMI
-                    | APPEND LPAREN ID COMMA NUMBER RPAREN SEMI'''
-    value = "%s.append(%s)" % (p[3], p[5])
-    p[0] = Node("append", None, value)
+def p_remove_expr(p):
+    '''remove : REMOVE LPAREN atom COMMA atom RPAREN'''
+    # Can be used in assignment stmt or alone
+    p[0] = Node("remove", [p[3], p[5]])
+    # MUST ADD AST ACTION
 
-# REMOVE
-def p_remove(p):
-    '''remove : remove_str
-              | remove_other'''
-    p[0] = Node("remove", [p[1]])
+# POP - Removes (and returns) item from specified position in list
+def p_pop_generic(p):
+    '''pop_generic : pop
+                   | pop SEMI'''
+    p[0] = Node("pop_generic", [p[1]])
 
-def p_remove_str(p):
-    '''remove_str : REMOVE LPAREN ID COMMA STRING RPAREN
-                  | REMOVE LPAREN ID COMMA STRING RPAREN SEMI'''
-    value = "%s.remove(\"%s\")" % (p[3], p[5])
-    p[0] = Node("remove", None, value)
-
-def p_remove_other(p):
-    '''remove_other : REMOVE LPAREN ID COMMA ID RPAREN
-                    | REMOVE LPAREN ID COMMA ID RPAREN SEMI
-                    | REMOVE LPAREN ID COMMA NUMBER RPAREN
-                    | REMOVE LPAREN ID COMMA NUMBER RPAREN SEMI'''
-    value = "%s.remove(%s)" % (p[3], p[5])
-    p[0] = Node("remove", None, value)
-
-# POP
 def p_pop(p):
-    '''pop : pop_default
-           | pop_arg'''
-    p[0] = Node("pop", [p[1]])
-
-def p_pop_default(p):
-    '''pop_default : POP LPAREN ID RPAREN
-                   | POP LPAREN ID RPAREN SEMI'''
-    value = "%s.pop()" % (p[3])
-    p[0] = Node("pop", None, value)
-
-def p_pop_arg(p):
-    '''pop_arg : POP LPAREN ID COMMA NUMBER RPAREN
-               | POP LPAREN ID COMMA NUMBER RPAREN SEMI'''
-    value = "%s.pop(%s)" % (p[3], p[5])
-    p[0] = Node("pop", None, value)
+    '''pop : POP LPAREN atom RPAREN
+           | POP LPAREN atom COMMA atom RPAREN'''
+    if len(p) == 5:
+        p[0] = Node("pop", [p[3]])
+    elif len(p) == 7:
+        p[0] = Node("pop", [p[3], p[5]])
+    # MUST ADD AST ACTION
