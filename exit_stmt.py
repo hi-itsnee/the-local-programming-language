53bb9a42d830056e508028e16a0dd34f1f6e6088
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-20 03:45:41 +0000

    untabified

--- exit_stmt.py
+++ exit_stmt.py
@@ -1,7 +1,6 @@
 from localast import Node
 
 def p_exit_stmt(p):
-	'''exit_stmt : EXIT LPAREN NUMBER RPAREN SEMI'''
-	
-	line = "exit(%s)\n" % (p[3])
-	p[0] = Node("exit", None, None, line)
+    '''exit_stmt : EXIT LPAREN NUMBER RPAREN SEMI'''
+    line = "exit(%s)\n" % (p[3])
+    p[0] = Node("exit", None, None, line)
--- math_expr.py
+++ math_expr.py
@@ -2,15 +2,15 @@
 # Author:                Team 13
 # Description:           local parser math statements
 # Supported Language(s): Python 2.x
-# Time-stamp:            <2012-04-19 23:21:38 plt>
+# Time-stamp:            <2012-04-19 23:43:49 plt>
 
 from localast import Node
 from localast import var_names
 
 def p_math_expr(p):
     '''math_expr : math_expr PLUS math_term
-		 | math_expr MINUS math_term
-		 | math_term'''
+                 | math_expr MINUS math_term
+                 | math_term'''
     if len(p) == 2:
         p[0] = Node("math_expression", [p[1]])
     elif len(p) == 4:
@@ -22,30 +22,30 @@ def p_math_expr(p):
 def p_math_term(p):
     '''math_term : math_term TIMES math_factor
                  | math_term DIVIDE math_factor
-		 | math_factor'''
+                 | math_factor'''
     if len(p) == 4:
-	if p[2] == '*':
-	    p[0] = Node("math_term", [p[1], p[3]], None, "%s * %s")
-	elif p[2] == '/':
-	    p[0] = Node("math_term", [p[1], p[3]], None, "%s / %s")
+        if p[2] == '*':
+            p[0] = Node("math_term", [p[1], p[3]], None, "%s * %s")
+        elif p[2] == '/':
+            p[0] = Node("math_term", [p[1], p[3]], None, "%s / %s")
     elif len(p) == 2:
-	p[0] = Node("math_term", [p[1]])
+        p[0] = Node("math_term", [p[1]])
 
 def p_math_factor(p):
     '''math_factor : LPAREN math_expr RPAREN
-		   | math_factor POWER math_expr
+                   | math_factor POWER math_expr
                    | ID
                    | NUMBER'''
-#		   | UMINUS math_factor '''
-#		   | math_factor MODULO math_expr
+#                  | UMINUS math_factor '''
+#                  | math_factor MODULO math_expr
 
     if len(p) == 4:
-	if p[1] == '(':
+        if p[1] == '(':
             p[0] = Node("math_factor", [p[2]], None, "(%s)")
-#	elif p[2] == '%':
-#	    p[0] = Node("math_factor", [p[1], p[3]], None,"%s '%' %s" )
-	elif p[2] == '^':
-	    p[0] = Node("math_factor", [p[1], p[3]], None, "%s ^ %s")
+#       elif p[2] == '%':
+#           p[0] = Node("math_factor", [p[1], p[3]], None,"%s '%' %s" )
+        elif p[2] == '^':
+            p[0] = Node("math_factor", [p[1], p[3]], None, "%s ^ %s")
     if len(p) == 2:
         p[0] = Node("math_factor", None, p[1])
 
