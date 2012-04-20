5db016d7ebe7b80e2feca3d769267a9a36f4b140
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-20 16:51:14 +0000

    added basic def_stmt (aayush to flesh out). Enabled PASS in lexer.  Added ability to stop execution after parsing (do not build AST)

--- local.py
+++ local.py
@@ -2,16 +2,18 @@
 # Author:                  Team 13
 # Description:             The local programming language compiler
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-04-19 22:44:31 plt>
+# Time-stamp:              <2012-04-20 12:48:47 plt>
 
 import argparse
 from localparse import parse
 from localast import walk_the_tree
 
-def main(filename, debug):
+def main(filename, debug, parse_only):
     # Read the source file and build the AST
     sourcecode = filename.read()
     ast = parse(sourcecode, debug)
+    if parse_only:
+        exit(0)
     if not ast:
         print "Error compiling"
         exit(1)
@@ -44,12 +46,15 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description='The local compiler.')
     argparser.add_argument('filename', metavar='FILENAME',
                            type=argparse.FileType('r'),
-                           help='the local sourcecode')
+                           help='The local sourcecode')
     argparser.add_argument('-d', '--debug', action='store_true', dest='debug',
                            default=False,
-                           help='enable debugging (default: False)')
+                           help='Enable debugging (default: false)')
+    argparser.add_argument('-p', '--parse', action='store_true', dest='parse',
+                           default=False,
+                           help='Exit after parsing (default: false)')
     try:
         args = argparser.parse_args()
-        main(args.filename, args.debug)
+        main(args.filename, args.debug, args.parse)
     except Exception as e:
         argparser.error(str(e))
