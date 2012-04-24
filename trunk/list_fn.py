41b4c9d67ad7c480cd3d72bf17e06583e0cb6277
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-24 15:01:39 +0000

    changed name

---
+++ trunk/list_fn.py
@@ -0,0 +1,47 @@
+# Filename:                list_fn.py
+# Author:                  Team 13
+# Description:             local language parser list functions
+# Supported Lanauge(s):    Python 2.x
+# Time-stamp:              <2012-04-24 10:44:43 plt>
+
+from localast import Node
+# Node(type, children=None, value=None, line=None)
+
+def p_list_fn(p):
+    '''list_fn : append
+               | remove_generic
+               | pop_generic'''
+    p[0] = Node("list_fn", [p[1]])
+
+# APPEND - Appends item to end of list
+def p_append(p):
+    '''append : APPEND LPAREN atom COMMA atom RPAREN SEMI'''
+    p[0] = Node("append", [p[3], p[5]])
+    # MUST ADD AST ACTION
+
+# REMOVE - Removes (and returns) specified item from list
+def p_remove_generic(p):
+    '''remove_generic : remove
+                      | remove SEMI'''
+    p[0] = Node("remove_generic", [p[1]])
+
+def p_remove_expr(p):
+    '''remove : REMOVE LPAREN atom COMMA atom RPAREN'''
+    # Can be used in assignment stmt or alone
+    p[0] = Node("remove", [p[3], p[5]])
+    # MUST ADD AST ACTION
+
+# POP - Removes (and returns) item from specified position in list
+def p_pop_generic(p):
+    '''pop_generic : pop
+                   | pop SEMI'''
+    p[0] = Node("pop_generic", [p[1]])
+
+def p_pop(p):
+    '''pop : POP LPAREN atom RPAREN
+           | POP LPAREN atom COMMA atom RPAREN'''
+    if len(p) == 5:
+        p[0] = Node("pop", [p[3]])
+    elif len(p) == 7:
+        p[0] = Node("pop", [p[3], p[5]])
+    # MUST ADD AST ACTION
