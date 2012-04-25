214c8ccba7da2835ae1b002acb5bd8787533c051
neerajaramanan@gmail.com <neerajaramanan@gmail.com@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-25 16:34:09 +0000

    Added dist function

--- trunk/coord_fn.py
+++ trunk/coord_fn.py
@@ -4,15 +4,27 @@
 # Supported Lanauge(s):    Python 2.x
 # Time-stamp:              <2012-04-24 10:53:41 plt>
 
+#import math
 from localast import Node
 # Node(type, children=None, value=None, line=None)
 
 def p_coord_fn(p):
     '''coord_fn : dist
                 | convertdist'''
+    p[0] = Node("Coord_fn", [p[1]])
 
 def p_dist(p):
-    '''dist : DIST'''
+    '''dist : dist_id
+            | dist_coord'''
+    p[0] = Node("Dist",[p[1]])
+
+def p_dist_id(p):
+    '''dist_id : DIST LPAREN ID COMMA ID RPAREN'''
+    p[0] = Node("Dist", None,"haversine.haversine(%s,%s)"%(p[3],p[5]))
+
+def p_dist_coord(p):
+    '''dist_coord : DIST LPAREN COORD COMMA COORD RPAREN'''
+    p[0] = Node("Dist", None,"haversine.haversine(\"%s\",\"%s\")"%(p[3],p[5]))
 
 def p_convertdist(p):
     '''convertdist : CONVERTDIST'''
