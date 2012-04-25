4d8d0ca42307928f57d88fe26f36d7107f772888
neerajaramanan@gmail.com <neerajaramanan@gmail.com@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-25 16:35:55 +0000

    Dist calls this

---
+++ trunk/haversine.py
@@ -0,0 +1,24 @@
+# Filename:                haversine.py
+# Author:                  Team 13
+# Description:             local language parser list functions
+# Supported Lanauge(s):    Python 2.x
+# Time-stamp:              <2012-04-24 10:53:41 plt>
+
+import math
+import sys
+# Node(type, children=None, value=None, line=None)
+
+def haversine(a,b):
+    alat = math.radians(a[0])
+    alon = math.radians(a[1])
+    blat = math.radians(b[0])
+    blon = math.radians(b[1])
+    hlat = alat - blat
+    hlon = alon - blon
+    hlat = (1-math.cos(hlat)) / 2
+    hlon = (1-math.cos(hlon)) / 2
+    foo = math.cos(alat) * math.cos(blat) * hlon
+    foo = foo + hlat
+    radius = 6371
+    d = 2 * radius * math.asin(math.sqrt(foo))
+    return d
