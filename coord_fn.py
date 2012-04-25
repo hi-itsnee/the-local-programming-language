7f1e6450f98e15ba8a6bddec69d618920ebba554
neerajaramanan@gmail.com <neerajaramanan@gmail.com@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-25 17:32:00 +0000

    Dist corrected and updated

--- coord_fn.py
+++ coord_fn.py
@@ -7,6 +7,7 @@
 #import math
 from localast import Node
 # Node(type, children=None, value=None, line=None)
+import re
 
 def p_coord_fn(p):
     '''coord_fn : dist
@@ -20,11 +21,42 @@ def p_dist(p):
 
 def p_dist_id(p):
     '''dist_id : DIST LPAREN ID COMMA ID RPAREN'''
-    p[0] = Node("Dist", None,"haversine.haversine(%s,%s)"%(p[3],p[5]))
+    value = "0"
+    value = value + "\nalat = math.radians(%s[0])" % (p[3])
+    value = value + "\nalon = math.radians(%s[1])" % (p[3])
+    value = value + "\nblat = math.radians(%s[0])" % (p[5])
+    value = value + "\nblon = math.radians(%s[1])" % (p[5])
+    value = value + "\nhlat = alat - blat"
+    value = value + "\nhlon = alon - blon"
+    value = value + "\nhlat = (1-math.cos(hlat)) / 2"
+    value = value + "\nhlon = (1-math.cos(hlon)) / 2"
+    value = value + "\nfoo = math.cos(alat) * math.cos(blat) * hlon"
+    value = value + "\nfoo = foo + hlat"
+    value = value + "\nradius = 6371"
+    value = value + "\nd = 2 * radius * math.asin(math.sqrt(foo))"
+    value = value + "\nreturn d"
+    p[0] = Node("Dist", None,value)
 
 def p_dist_coord(p):
     '''dist_coord : DIST LPAREN COORD COMMA COORD RPAREN'''
-    p[0] = Node("Dist", None,"haversine.haversine(\"%s\",\"%s\")"%(p[3],p[5]))
+    value = "0"
+    mo = re.search('(?P<lat>[+-]?\d+\.\d+)\s*,\s*(?P<longi>[+-]?\d+\.\d+)', p[3])
+    value = value + "\nalat = math.radians(%f)" % (float(mo.group('lat')),)
+    value = value + "\nalon = math.radians(%f)" % (float(mo.group('longi')),)
+    mo = re.search('(?P<lat>[+-]?\d+\.\d+)\s*,\s*(?P<longi>[+-]?\d+\.\d+)', p[5])
+    value = value + "\nblat = math.radians(%f)" % (float(mo.group('lat')),) 
+    value = value + "\nblon = math.radians(%f)" % (float(mo.group('longi')),)
+    value = value + "\nhlat = alat - blat"
+    value = value + "\nhlon = alon - blon"
+    value = value + "\nhlat = (1-math.cos(hlat)) / 2"
+    value = value + "\nhlon = (1-math.cos(hlon)) / 2"
+    value = value + "\nfoo = math.cos(alat) * math.cos(blat) * hlon"
+    value = value + "\nfoo = foo + hlat"
+    value = value + "\nradius = 6371"
+    value = value + "\nd = 2 * radius * math.asin(math.sqrt(foo))"
+    value = value + "\nreturn d"
+    p[0] = Node("Dist", None,value)
+
 
 def p_convertdist(p):
     '''convertdist : CONVERTDIST'''
