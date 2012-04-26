1b71256b5dcabb3955d6758140004503dd11fa0f
neerajaramanan@gmail.com <neerajaramanan@gmail.com@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-26 16:26:35 +0000

    Fixed multiple line print and added to jump statement

--- jump_stmt.py
+++ jump_stmt.py
@@ -16,18 +16,22 @@ def p_jump_stmt(p):
 def p_cont_jump_stmt(p):
     '''cont_jump_stmt : CONTINUE SEMI'''
     value = "continue"
-    p[0] = Node("continue", None, value)
+    p[0] = Node("continue", None, value, value)
 
 def p_break_jump_stmt(p):
     '''break_jump_stmt : BREAK SEMI'''
     value = "break"
-    p[0] = Node("break", None, value)
+    p[0] = Node("break", None, value, value)
 
 def p_pass_jump_stmt(p):
     '''pass_jump_stmt : PASS SEMI'''
     value = "pass"
-    p[0] = Node("pass", None, value)
+    p[0] = Node("pass", None, value, value)
 
 def p_return_jump_stmt(p):
-    '''return_jump_stmt : RETURN expr SEMI'''
-    p[0] = Node("return", [p[2]])
+    '''return_jump_stmt : RETURN expr SEMI
+                        | RETURN SEMI'''
+    if len(p) == 4:
+        p[0] = Node("return", [p[2]], None, "return %s")
+    else:
+       p[0] = Node("return", None, None, "return")
