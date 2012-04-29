8bb63e4526ae1d9ce5ee8fe007ca3aeb9c0e6de5
Julianna.Eum <Julianna.Eum@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-29 01:26:34 +0000

    added script for testing - just type ./test to run it...  most of the test statements are not working. :(

--- trunk/testing/testinputs.py
+++ trunk/testing/testinputs.py
@@ -1,41 +1,79 @@
 ##### Input .local files for testing
 
 ### LEXING/PARSING TESTS:
-# cond_stmt.py local test inputs:
-if_test = '''if a
-    print("a is true");'''
-
-ifelse_test = '''if a
-    print("a is true");
-else
-    print("a is false");'''
-
-nesting_test = '''if a {
-    print("a is true");
-    if b
-        print("made it here");
-    else
-        print("made it there");
-}
-else
-    print("a is false");'''
-
-# math_expr.py local test inputs:
-math_test1 = '''a = isd + dsa * 12 + (7/5);'''
 
 # assign_stmt.py local test inputs:
 assign_test = '''a = 10;
-b = 0;'''
+b = (0 + 1);'''
+#c *= a;
+#d /= a;
+#e %= 2;
+#f += 4;
+#g -= 5;
+#h and= a;
+#i or= b;
+
+# cond_stmt.py local test inputs:
+if_test = '''a = 5;
+if a > 3
+    print(a > 3);'''
+
+ifelse_test = '''a = True;
+if a
+    print(a is true);
+else
+    print(a is false);'''
+
+nesting_test = '''if a {
+    print(a is true);
+    if b == a
+        print(made it here);
+    elif b < a
+        print(made it there);
+    else
+        print(made it everywhere);
+}
+else
+    print(a is false);'''
 
 # exit
 exit_test = '''exit(1);'''
 
-# expr
+# expr.py python outputs:
+binop_test = '''a = 1 + 1;
+b = 2 - 2;
+c = 3 * 3;
+d = 4 / 4;
+e = 5 % 2;
+f = 6 ^ 2;
+r = a or b;
+s = c and d;
+t = e < f;
+u = a <= b;
+v = c > d;
+w = e >= f;
+x = a == b;
+y = c != d;
+z = (e);'''
+
+indices_test = '''b = a[1];'''
+
+unary_test = '''a = True;
+b = not a;
+c = -5;
+d = c++;
+e = c--;'''
+
+# io_stmt.py local test inputs:
+io_test = '''open("../sandbox/array.local")'''
+
+# math_expr.py local test inputs:
+math_test1 = '''a = isd + dsa * 12 + (7/5);'''
 
 
-### BLACK BOX TESTS:
+### PROGRAM TESTS:
 # Hello World:
-helloworld_test = '''print("Hello, world!");'''
+helloworld_test = '''print(Hello, world!);'''
 
 # example1.py local test inputs:
 example1_test = '''x = 1;
