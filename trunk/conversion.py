fe8d4622578172128c62dc332d4df76d2cf08dca
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-30 03:28:31 +0000

    Added functionality to add import statements to target code. Made names more intuitive. Minor cleanup.

--- trunk/conversion.py
+++ trunk/conversion.py
@@ -1,27 +1,23 @@
 # Filename:                haversine.py
 # Author:                  Team 13
-# Description:             local language parser list functions
+# Description:             local language conversion module
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-04-24 10:53:41 plt>
+# Time-stamp:              <2012-04-29 23:26:02 plt>
 
-import math
-import sys
-# Node(type, children=None, value=None, line=None)
-
-def fly(a, b, c = "m"):
+def convertdist(a, b, c="m"):
     if (c == "ft"):
-        if ( b == "m"):
-            a =  a / 3.281
+        if (b == "m"):
+            a = a / 3.281
         elif (b == "mi"):
             a = a / 5280.00
     elif (c == "mi"):
-        if ( b == "m"):
+        if (b == "m"):
             a = a * 1609.3
         elif ( b == "ft"):
-            a = a * 5280
+            a = a * 5280.0
     elif (c == "m"):
         if ( b == "mi"):
             a = a / 1609.3
-        elif ( b == "ft"):
+        elif (b == "ft"):
             a = a * 3.281
     return a
--- trunk/local.py
+++ trunk/local.py
@@ -2,13 +2,17 @@
 # Author:                  Team 13
 # Description:             The local programming language compiler
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-04-20 12:48:47 plt>
+# Time-stamp:              <2012-04-29 23:22:45 plt>
 
 import argparse
 from localparse import parse
 from localast import walk_the_tree
 
 def main(filename, debug, parse_only):
+    # Initialize variables
+    import_convertdist = False
+    import_dist = False
+    import_sys = False
     # Read the source file and build the AST
     sourcecode = filename.read()
     ast = parse(sourcecode, debug)
@@ -18,10 +22,25 @@ def main(filename, debug, parse_only):
         print "Error compiling"
         exit(1)
     # Walk the AST to produce target (Python) code
-    code = walk_the_tree(ast, debug=debug).split("\n")
+    code = walk_the_tree(ast, debug=debug)
+    # Determine if we need to import anything
+    if code.find("convertdist(") != -1:
+        import_convertdist = True
+    if code.find(" dist(") != -1:
+        import_dist = True
+    if code.find("argv[") != -1:
+        import_sys = True
+    # Split on newlines, then we'll remove emtpy lines
+    code = code.split("\n")
     # One can go insane making newlines/empty lines perfect throughout the
     # compiler, or they can be stripped out here
     clean_code = [ ]
+    if import_convertdist:
+        clean_code.append("from conversion import convertdist")
+    if import_dist:
+        clean_code.append("from haversine import dist")
+    if import_sys:
+        clean_code.append("import sys")
     for line in code:
         if not line.strip():
             # Blank line
