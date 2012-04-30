d639ce58c936d722510a5f461518d3af80052ebb
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-04-30 00:16:54 +0000

    print now uses atom.  format print uses same arglist as def (and same/similar ast functions)

--- trunk/def_stmt.py
+++ trunk/def_stmt.py
@@ -2,7 +2,7 @@
 # Author:                  Team 13
 # Description:             local parse def statement
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-04-29 19:08:32 plt>
+# Time-stamp:              <2012-04-29 20:15:36 plt>
 
 from localast import Node
 
@@ -10,6 +10,7 @@ def p_def(p):
     '''def_stmt : DEF ID LPAREN arglist RPAREN LBRACE stmt_list RBRACE'''
     p[0] = Node("def", [p[4], p[7]], p[2])
 
+# Note: arglist also used in print_stmt.py
 def p_arglist(p):
     '''arglist : arglist COMMA atom
                | atom'''
--- trunk/print_stmt.py
+++ trunk/print_stmt.py
@@ -2,7 +2,7 @@
 # Author:                  Team 13
 # Description:             local parser print statement
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-04-24 13:18:16 plt>
+# Time-stamp:              <2012-04-29 20:14:57 plt>
 
 from localast import Node
 #Node(type, children=Node, value=None, line=None)
@@ -10,23 +10,14 @@ from localast import Node
 def p_print_stmt(p):
     '''print_stmt : simple_print_stmt
                   | format_print_stmt'''
-    p[0] = Node("print1", [p[1]])
+    p[0] = Node("print_stmt", [p[1]])
 
 def p_simple_print_stmt(p):
-    '''simple_print_stmt : PRINT LPAREN STRING RPAREN SEMI'''
-    value = "print \"%s\"\n" % p[3]
-    p[0] = Node("simpleprint", None, value)
+    '''simple_print_stmt : PRINT LPAREN atom RPAREN SEMI'''
+    line = "print %s\n"
+    p[0] = Node("simple_print", [p[3]], None, line)
 
 def p_format_print_stmt(p):
-    '''format_print_stmt : PRINT LPAREN STRING COMMA format_stmt RPAREN SEMI'''
-    value = "print \"%s\"" % p[3]
-    p[0] = Node("format_print_statement", [p[5]], value)
-
-def p_format_stmt(p):
-    '''format_stmt : format_stmt COMMA STRING
-                   | STRING'''
-    if len(p) == 4:
-        p[0] = Node("format_statement", [p[1]], p[3], None)
-    if len(p) == 2:
-        value = p[1]
-        p[0] = Node("format_statement", None, value)
+    '''format_print_stmt : PRINT LPAREN atom COMMA arglist RPAREN SEMI'''
+    # Note: arglist defined in def_stmt.py
+    p[0] = Node("format_print", [p[3], p[5]])
