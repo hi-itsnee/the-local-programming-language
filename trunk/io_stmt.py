f8ce80cabc173873ff28961d29e93b30a0a6ddad
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-05-01 15:05:33 +0000

    removed SEMI needed from productions: open and read only used in assignments, which have SEMIs already

--- trunk/io_stmt.py
+++ trunk/io_stmt.py
@@ -2,7 +2,7 @@
 # Author:                  Team 13
 # Description:             local parser I/O statements
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-04-24 13:13:46 plt>
+# Time-stamp:              <2012-05-01 11:04:57 plt>
 
 from localast import Node
 # Node(type, children=None, value=None, line=None)
@@ -14,8 +14,8 @@ def p_io_stmt(p):
     p[0] = Node("io_stmt", [p[1]])
 
 def p_open_stmt(p):
-    '''open_stmt : OPEN LPAREN STRING RPAREN SEMI
-                 | OPEN LPAREN STRING COMMA STRING RPAREN SEMI'''
+    '''open_stmt : OPEN LPAREN STRING RPAREN
+                 | OPEN LPAREN STRING COMMA STRING RPAREN'''
     filename = p[3]
     if len(p) == 6:
         # Default is read (r)
@@ -25,7 +25,9 @@ def p_open_stmt(p):
     value = "open(\"%s\", \"%s\")" % (filename, mode)
     p[0] = Node("open", None, value)
 
+# PRINT grammar in print_stmt.py
+
 def p_read_stmt(p):
-    '''read_stmt : READ LPAREN RPAREN SEMI'''
+    '''read_stmt : READ LPAREN RPAREN'''
 
     p[0] = Node("read_stmt", None, "raw_input()")
