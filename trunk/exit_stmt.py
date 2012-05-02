84bc74f6a91ba4056e79732039a8f34ce60fa06f
neerajaramanan@gmail.com <neerajaramanan@gmail.com@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-05-02 23:59:05 +0000

    Removed SEMI from everywhere. Note: Whoever is doing list_fn.py has to deal with it in that file

--- trunk/exit_stmt.py
+++ trunk/exit_stmt.py
@@ -7,6 +7,6 @@
 from localast import Node
 
 def p_exit_stmt(p):
-    '''exit_stmt : EXIT LPAREN NUMBER RPAREN SEMI'''
+    '''exit_stmt : EXIT LPAREN NUMBER RPAREN'''
     line = "exit(%s)\n" % (p[3])
     p[0] = Node("exit", None, line, line)
