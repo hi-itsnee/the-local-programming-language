8b6393293a0fae83092aa70121dd87504906cba4
neerajaramanan@gmail.com <neerajaramanan@gmail.com@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-05-02 06:41:23 +0000

    Completed IO statements. Subdivided as io_fn and io_stmt. Parses command line read as user raw_input

--- trunk/io_stmt.py
+++ trunk/io_stmt.py
@@ -8,26 +8,33 @@ from localast import Node
 # Node(type, children=None, value=None, line=None)
 
 def p_io_stmt(p):
-    '''io_stmt : open_stmt
-               | print_stmt
-               | read_stmt'''
+    '''io_stmt : print_stmt
+               | close_stmt'''
     p[0] = Node("io_stmt", [p[1]])
 
+def p_io_fn(p):
+    '''io_fn : open_stmt
+             | read_stmt'''
+    p[0] = Node("io_fn", [p[1]])
+     
 def p_open_stmt(p):
     '''open_stmt : OPEN LPAREN STRING RPAREN
                  | OPEN LPAREN STRING COMMA STRING RPAREN'''
     filename = p[3]
-    if len(p) == 6:
+    if len(p) == 5:
         # Default is read (r)
         mode = "r"
-    elif len(p) == 8:
+    elif len(p) == 7:
         mode = p[5]
-    value = "open(\"%s\", \"%s\")" % (filename, mode)
-    p[0] = Node("open", None, value)
+    value = "open(%s, \"%s\")" % (filename, mode)
+    p[0] = Node("open", None, value, value)
 
 # PRINT grammar in print_stmt.py
 
-def p_read_stmt(p):
-    '''read_stmt : READ LPAREN RPAREN'''
+def p_close_stmt(p):
+    '''close_stmt : CLOSE LPAREN atom RPAREN SEMI'''
+    p[0] = Node("close", [p[3]], None, "%s.close()")
 
-    p[0] = Node("read_stmt", None, "raw_input()")
+def p_read_stmt(p):
+    '''read_stmt : READ LPAREN atom RPAREN'''
+    p[0] = Node("read_stmt", [p[3]], None,"raw_input(%s)")
