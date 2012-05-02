3413615d034c39dae02327521010ac668433f5db
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-05-02 21:33:07 +0000

    add double statements (++, --)

---
+++ trunk/double_stmt.py
@@ -0,0 +1,17 @@
+# Filename:                double_stmt.py
+# Author:                  Team 13
+# Description:             local parser double statments
+# Supported Lanauge(s):    Python 2.x
+# Time-stamp:              <2012-05-02 17:25:28 plt>
+
+from localast import Node
+
+def p_double_stmt(p):
+    ''' double_stmt : ID MINUSMINUS %prec MINUSMINUS
+                    | ID PLUSPLUS %prec PLUSPLUS'''
+    print p[2]
+    if p[2] == "++":
+        value = "%s += 1" % (p[1])
+    elif p[2] == "--":
+        value = "%s -= 1" % (p[1])
+    p[0] = Node("double", None, value)
