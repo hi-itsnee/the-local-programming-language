0a2df95ac37c6df3cdc8cf2f38e4788053689d53
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-05-03 23:28:08 +0000

    shouldn't have parens around output

--- double_stmt.py
+++ double_stmt.py
@@ -2,7 +2,7 @@
 # Author:                  Team 13
 # Description:             local parser double statments
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-05-02 17:25:28 plt>
+# Time-stamp:              <2012-05-03 19:25:30 plt>
 
 from localast import Node
 
@@ -10,7 +10,7 @@ def p_double_stmt(p):
     ''' double_stmt : ID MINUSMINUS %prec MINUSMINUS
                     | ID PLUSPLUS %prec PLUSPLUS'''
     if p[2] == "++":
-        value = "(%s += 1)" % (p[1])
+        value = "%s += 1" % (p[1])
     elif p[2] == "--":
-        value = "(%s -= 1)" % (p[1])
+        value = "%s -= 1" % (p[1])
     p[0] = Node("double", None, value)
