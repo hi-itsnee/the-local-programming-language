cbce1994f0df7a8fc7528814c0ca4cc74ab2fe2f
neerajaramanan@gmail.com <neerajaramanan@gmail.com@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-05-03 00:17:43 +0000

    Fixed pop and remove

--- trunk/list_fn.py
+++ trunk/list_fn.py
@@ -9,43 +9,21 @@ from localast import Node
 
 def p_list_fn(p):
     '''list_fn : append
-               | remove_generic
-               | pop_generic'''
+               | remove
+               | pop'''
     p[0] = Node("list_fn", [p[1]])
 
 # APPEND - Appends item to end of list
 def p_append(p):
-    '''append : APPEND LPAREN atom COMMA atom RPAREN SEMI'''
+    '''append : APPEND LPAREN atom COMMA atom RPAREN'''
     p[0] = Node("append", [p[3], p[5]], None, "%s.append(%s)")
-    # MUST ADD AST ACTION
 
 # REMOVE - Removes (and returns) specified item from list
-def p_remove_generic(p):
-    '''remove_generic : remove
-                      | remove SEMI'''
-    p[0] = Node("remove_generic", [p[1]])
-
-def p_remove_expr(p):
+def p_remove(p):
     '''remove : REMOVE LPAREN atom COMMA atom RPAREN'''
-    # Can be used in assignment stmt or alone
-    p[0] = Node("remove", [p[3], p[5]], None, "%s.remove(%s)")
-    # MUST ADD AST ACTION
+    p[0] = Node("remove", [p[3],p[5]], None, "%s.remove(%s)")
 
 # POP - Removes (and returns) item from specified position in list
-def p_pop_generic(p):
-    '''pop_generic : pop_assignment
-                   | pop SEMI'''
-    #Note : Previous production of pop_generic -> pop(for assignment) was being considered as pop SEMI(stand alone); thus causing an error while parsing
-    p[0] = Node("pop_generic", [p[1]])
-
-def p_pop_assignment(p):
-    '''pop_assignment : POP LPAREN atom RPAREN
-                      | POP LPAREN atom COMMA atom RPAREN'''
-    if len(p) == 5:
-        p[0] = Node("pop_assignment", [p[3]], None, "%s.pop()")
-    else:    
-        p[0] = Node("pop_assignment", [p[3], p[5]], None, "%s.pop(%s)")
-
 def p_pop(p):
     '''pop : POP LPAREN atom RPAREN
            | POP LPAREN atom COMMA atom RPAREN'''
@@ -53,4 +31,3 @@ def p_pop(p):
         p[0] = Node("pop", [p[3]], None, "%s.pop()")
     elif len(p) == 7:
         p[0] = Node("pop", [p[3], p[5]], None, "%s.pop(%s)")
-    # MUST ADD AST ACTION
