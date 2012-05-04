3d577d6eaefb11704d71443dc2d9e2bc17147e21
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-05-04 19:39:02 +0000

    grouped common functions

--- trunk/localast.py
+++ trunk/localast.py
@@ -2,7 +2,7 @@
 # Author:                  Team 13
 # Description:             local language AST utilities
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-05-04 15:24:59 plt>
+# Time-stamp:              <2012-05-04 15:38:22 plt>
 
 # Number of spaces a tab equals
 INDENT = 4
@@ -27,6 +27,49 @@ class Node:
         self.value = value
         self.line = line
 
+# Utility walker function
+def _do_arglist_subtree(node, code, debug):
+    '''For building a list of arguments'''
+    # A list of arguments
+    if len(node.children) == 2:
+        # Recursively follow the list
+        arglist = _do_arglist_subtree(node.children[0], code, debug)
+        # Evaluate the atom and append it
+        arg = walk_the_tree(node.children[1], code, debug)
+        arglist += ", " + arg
+        return arglist
+    elif len(node.children) == 1:
+        arg = walk_the_tree(node.children[0], code, debug)
+        return arg
+
+# Main statement block builder
+def _do_stmt_list_subtree(node, code, debug):
+    if len(node.children) == 2:
+        stmt_list = _do_stmt_list_subtree(node.children[0], code, debug)
+        stmt = walk_the_tree(node.children[1], code, debug)
+        return stmt_list + "\n" + stmt
+    elif len(node.children) == 1:
+        stmt = walk_the_tree(node.children[0], code, debug)
+        return stmt
+
+def _return_subtree(node, code, debug):
+    return_expr_child = walk_the_tree(node.children[0], code, debug)
+    return "return %s" % (return_expr_child)
+
+# Print subtree
+def _do_print_subtree(node, code, debug):
+    '''For building a format print statement'''
+    # Simple print
+    if len(node.children) == 1:
+        print_str = walk_the_tree(node.children[0], code, debug)
+        return "print %s\n" % (print_str)
+    # Format print
+    elif len(node.children) == 2:
+        print_str = walk_the_tree(node.children[0], code, debug)
+        print_arglist = _do_arglist_subtree(node.children[1], code, debug)
+        return "print %s %% (%s)\n" % (print_str, print_arglist)
+
+# Indented blocks
 def _do_if_subtree(node, code, debug):
     '''For indenting IF code blocks'''
     # Walk tree to build expression
@@ -113,21 +156,6 @@ def _do_def_subtree(node, code, debug):
     elif len(node.children) == 1:
         return "def %s():\n%s\n" % (node.value, def_stmt_i)
 
-
-def _do_arglist_subtree(node, code, debug):
-    '''For building a list of arguments'''
-    # A list of arguments
-    if len(node.children) == 2:
-        # Recursively follow the list
-        arglist = _do_arglist_subtree(node.children[0], code, debug)
-        # Evaluate the atom and append it
-        arg = walk_the_tree(node.children[1], code, debug)
-        arglist += ", " + arg
-        return arglist
-    elif len(node.children) == 1:
-        arg = walk_the_tree(node.children[0], code, debug)
-        return arg
-
 def _for_subtree(node, code, debug):
     '''For indenting For block'''
     for_atomchild = walk_the_tree(node.children[0], code, debug)
@@ -146,10 +174,6 @@ def _while_subtree(node, code, debug):
         while_stmt_indented += " "*INDENT + line + "\n"
     return "while %s:\n%s" % (while_exprchild, while_stmt_indented)
 
-def _return_subtree(node, code, debug):
-    return_expr_child = walk_the_tree(node.children[0], code, debug)
-    return "return %s" % (return_expr_child)
-
 def _except_subtree(node, code, debug):
     except_try_child = walk_the_tree(node.children[0], code, debug).split("\n")
     print node.children[1]
@@ -163,18 +187,6 @@ def _except_subtree(node, code, debug):
         except_catchchild_indented += " "*INDENT + line + "\n"
     print except_catch_child
     return "try:\n%s\nexcept %s:\n%s" % (except_trychild_indented, node.value, except_catchchild_indented)
-
-def _do_print_subtree(node, code, debug):
-    '''For building a format print statement'''
-    # Simple print
-    if len(node.children) == 1:
-        print_str = walk_the_tree(node.children[0], code, debug)
-        return "print %s\n" % (print_str)
-    # Format print
-    elif len(node.children) == 2:
-        print_str = walk_the_tree(node.children[0], code, debug)
-        print_arglist = _do_arglist_subtree(node.children[1], code, debug)
-        return "print %s %% (%s)\n" % (print_str, print_arglist)
 
 def _do_indent_subtree(node, code, debug):
     '''For indenting statements within blocks'''
@@ -203,15 +215,7 @@ def _do_indent_subtree(node, code, debug):
     if node.type == 'except':
         return _except_subtree(node, code, debug)
 
-def _do_stmt_list_subtree(node, code, debug):
-    if len(node.children) == 2:
-        stmt_list = _do_stmt_list_subtree(node.children[0], code, debug)
-        stmt = walk_the_tree(node.children[1], code, debug)
-        return stmt_list + "\n" + stmt
-    elif len(node.children) == 1:
-        stmt = walk_the_tree(node.children[0], code, debug)
-        return stmt
-
+# Main tree-walking function
 def walk_the_tree(node, code="", debug=False):
     '''Walk the AST and return a string, which is a Python program'''
     # Default case (should not be hit)
