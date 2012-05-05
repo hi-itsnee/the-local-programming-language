335cac1be9d0fc75dfcaea5a61b16b01cd07db8e
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-05-05 22:23:34 +0000

    need to take string coordinate and turn the parts into floats

--- haversine.py
+++ haversine.py
@@ -2,17 +2,17 @@
 # Author:                  Team 13
 # Description:             local language Haversine module
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-05-05 16:28:06 plt>
+# Time-stamp:              <2012-05-05 17:44:24 plt>
 
 import math
 
-def dist(a, b):
-    a = float(a)
-    b = float(b)
-    alat = math.radians(a[0])
-    alon = math.radians(a[1])
-    blat = math.radians(b[0])
-    blon = math.radians(b[1])
+def dist(c1, c2):
+    lat1, lon1 = map(lambda x: float(x), c1.strip()[1:-1].split(","))
+    lat2, lon2 = map(lambda x: float(x), c2.strip()[1:-1].split(","))
+    alat = math.radians(lat1)
+    alon = math.radians(lon1)
+    blat = math.radians(lat2)
+    blon = math.radians(lon2)
     hlat = alat - blat
     hlon = alon - blon
     hlat = (1 - math.cos(hlat)) / 2
