bf7505e258049a0366ecd84719faaf7787607575
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-05-05 21:05:21 +0000

    make sure haversine converts a and b to floats

--- haversine.py
+++ haversine.py
@@ -2,19 +2,21 @@
 # Author:                  Team 13
 # Description:             local language Haversine module
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-04-29 23:26:43 plt>
+# Time-stamp:              <2012-05-05 16:28:06 plt>
 
 import math
 
 def dist(a, b):
+    a = float(a)
+    b = float(b)
     alat = math.radians(a[0])
     alon = math.radians(a[1])
     blat = math.radians(b[0])
     blon = math.radians(b[1])
     hlat = alat - blat
     hlon = alon - blon
-    hlat = (1-math.cos(hlat)) / 2
-    hlon = (1-math.cos(hlon)) / 2
+    hlat = (1 - math.cos(hlat)) / 2
+    hlon = (1 - math.cos(hlon)) / 2
     foo = math.cos(alat) * math.cos(blat) * hlon
     foo = foo + hlat
     radius = 6371
