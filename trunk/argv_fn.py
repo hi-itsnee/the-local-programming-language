d02a1d7a586190fd1408dfc2543268e3a5145278
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-05-05 17:54:37 +0000

    list moved from lexer to parser (balanced brackets are CF, not regular).  indexes no longer specified with ?.  argv adjusted as well

--- trunk/argv_fn.py
+++ trunk/argv_fn.py
@@ -1,8 +1,12 @@
+# Filename:                argv_fn.py
+# Author:                  Team 13
+# Description:             local parser argv function
+# Supported Lanauge(s):    Python 2.x
+# Time-stamp:              <2012-05-05 13:44:51 plt>
+
 from localast import Node
 
 def p_argv_fn(p):
-    '''argv_fn : ARGV LIST'''
-    s = p[2].strip('[]')
-    s = s.strip('\'\'')
-    value = "sys.argv[%s]" % s
-    p[0] = Node("argv", None, value) 
+    '''argv_fn : ARGV list'''
+    line = "sys.argv%s"
+    p[0] = Node("argv", [p[2]], None, line)
--- trunk/locallex.py
+++ trunk/locallex.py
@@ -2,7 +2,7 @@
 # Author:                  Team 13
 # Description:             The local programming language lexer
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-05-04 16:59:03 plt>
+# Time-stamp:              <2012-05-05 13:51:54 plt>
 
 import ply.lex as lex
 import re
@@ -24,10 +24,11 @@ tokens = reserved + (
     'PLUSPLUS', 'MINUSMINUS', 'LT', 'LE', 'GT', 'GE', 'EQ', 'NE',
 
     # Delimeters
-    'SEMI', 'LPAREN', 'RPAREN', 'COMMA', 'LBRACE', 'RBRACE', 'QUESTION',
+    'SEMI', 'LPAREN', 'RPAREN', 'COMMA', 'LBRACE', 'RBRACE',
+    'LBRACKET', 'RBRACKET',
 
-    # Literals
-    'ID', 'COORD', 'STRING', 'NUMBER', 'BOOL', 'NULL', 'LIST',
+    # Literals ('list' is in parser, not lexer)
+    'ID', 'COORD', 'STRING', 'NUMBER', 'BOOL', 'NULL',
     )
 
 # Ignore whitespace
@@ -48,12 +49,12 @@ def t_COORD(t):
     t.value = str(mw)
     return t
 
-def t_LIST(t):
-    r'\[\s*\]|\[\s*[a-zA-Z0-9_\"\(\)\[\]]+\s*(,\s*[a-zA-Z0-9_\"\(\)\[\]]+\s*)*\]'
-    liststr = t.value[1:-1]
-    listlist = liststr.split(",")
-    t.value = "[" + ", ".join(map(lambda x: x.strip(), listlist)) + "]"
-    return t
+# def t_LIST(t):
+#     r'\[\s*\]|\[\s*[a-zA-Z0-9_\"\(\)\[\]\.\,]+\s*(,\s*[a-zA-Z0-9_\"\(\)\[\]\.\,]+\s*)*\]'
+#     liststr = t.value[1:-1]
+#     listlist = liststr.split(",")
+#     t.value = "[" + ", ".join(map(lambda x: x.strip(), listlist)) + "]"
+#     return t
 
 def t_NUMBER(t):
     r'(\d+(\.\d*)?|\.\d+)([eE][-+]? \d+)?'
@@ -106,9 +107,10 @@ t_LPAREN           = r'\('
 t_RPAREN           = r'\)'
 t_LBRACE           = r'\{'
 t_RBRACE           = r'\}'
+t_LBRACKET         = r'\['
+t_RBRACKET         = r'\]'
 t_COMMA            = r','
 t_SEMI             = r';'
-t_QUESTION         = r'\?'
 
 # Identifiers and reserved words
 reserved_map = { }
