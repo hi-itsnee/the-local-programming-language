362270d91dfd70ea662a80deac3f96eda79b3feb
michael7costello <michael7costello@49c92f30-f5ba-5058-53fa-d6cf02efde4c>
2012-05-06 00:24:06 +0000

    added string join function

--- str_fn.py
+++ str_fn.py
@@ -2,7 +2,7 @@
 # Author:                  Team 13
 # Description:             local parser string functions
 # Supported Lanauge(s):    Python 2.x
-# Time-stamp:              <2012-05-05 19:00:13 plt>
+# Time-stamp:              <2012-05-05 20:23:15 plt>
 
 from localast import Node
 # Node(type, children=None, value=None, line=None)
@@ -11,20 +11,21 @@ def p_str_fn(p):
     '''str_fn : len
               | split
               | strip
-              | str'''
+              | str
+              | join'''
     p[0] = Node("str_fn", [p[1]])
 
-#LEN
+# LEN
 def p_len(p):
     '''len : LEN LPAREN atom RPAREN'''
     p[0] = Node("len", [p[3]], None, "len(%s)")
 
-#SPLIT
+# SPLIT
 def p_split(p):
     '''split : SPLIT LPAREN atom COMMA atom RPAREN'''
     p[0] = Node("split", [p[3], p[5]], None, "%s.split(%s)")
 
-#STRIP
+# STRIP
 def p_strip(p):
     '''strip : STRIP LPAREN atom COMMA atom RPAREN
              | STRIP LPAREN atom RPAREN'''
@@ -33,7 +34,13 @@ def p_strip(p):
     else:
         p[0] = Node("strip", [p[3]], None, "%s.strip()")
 
-#STR
+# STR
 def p_str(p):
     '''str : STR LPAREN atom RPAREN'''
     p[0] = Node("str", [p[3]], None, "str(%s)")
+
+# JOIN
+def p_join(p):
+    '''join : JOIN LPAREN atom COMMA atom RPAREN'''
+    # Note the order of the children (need to swap between local and Python)
+    p[0] = Node("join", [p[5], p[3]], None, "%s.join(%s)")
